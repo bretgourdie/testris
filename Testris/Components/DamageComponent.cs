554b710cd7f58e61d8f8fb460caf6b1409cd5b55
Bret Gourdie <bretgourdie@gmail.com>
2019-01-04 14:06:38 -0500

    Added DamageComponent

---
+++ Testris/Components/DamageComponent.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Nez;
+
+namespace Testris.Components
+{
+    public class DamageComponent : Component
+    {
+        SimpleMover _mover;
+
+        private const float reducedSpeed = 70f;
+
+        public override void onAddedToEntity()
+        {
+            _mover = this.entity.getComponent<SimpleMover>();
+
+            Core.schedule(2f, t => takeDamageTwo());
+        }
+
+        public void takeDamage()
+        {
+            _mover.speed = reducedSpeed;
+
+            Core.schedule(2f, t => _mover.speed = 100f);
+        }
+
+        public void takeDamageTwo()
+        {
+            _mover.speed = reducedSpeed;
+
+            Core.startCoroutine(resetSpeedAfterDelay());
+        }
+
+        IEnumerator resetSpeedAfterDelay()
+        {
+            yield return Coroutine.waitForSeconds(2f);
+
+            _mover.speed = 100f;
+        }
+    }
+}
