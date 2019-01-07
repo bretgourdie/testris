d3a20778c3443c7582e644b104c807d2e721c9e0
Bret Gourdie <bretgourdie@gmail.com>
2019-01-07 14:43:11 -0500

    Extend from Nez Component

--- Testris/GameObject/Tetromino/Piece.cs
+++ Testris/GameObject/Tetromino/Piece.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Nez;
 
 namespace Testris.GameObject.Tetromino
 {
-    public abstract class Piece
+    public abstract class Piece : Component
     {
         public abstract Color Color { get; }
     }
