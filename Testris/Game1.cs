d08bb50da974cc56f2bfc3bf46b2a478c0e58920
Bret Gourdie <bretgourdie@gmail.com>
2019-01-07 15:16:37 -0500

    Added sample sprite and positions for them

--- Testris/Game1.cs
+++ Testris/Game1.cs
@@ -29,23 +29,37 @@ namespace Testris
 
             var myScene = Scene.createWithDefaultRenderer(Color.CornflowerBlue);
 
+            var tetromino = myScene.content.Load<Texture2D>("Textures/tetromino");
+
             var ipiece = myScene.createEntity("lpiece");
             ipiece.addComponent<IPiece>();
+            ipiece.addComponent(new Sprite(tetromino));
+            ipiece.position = new Vector2(100, 100);
 
             var jpiece = myScene.createEntity("jpiece");
             jpiece.addComponent<JPiece>();
+            jpiece.addComponent(new Sprite(tetromino));
+            jpiece.position = new Vector2(100, 150);
 
             var lpiece = myScene.createEntity("lpiece");
             lpiece.addComponent<LPiece>();
+            lpiece.addComponent(new Sprite(tetromino));
+            lpiece.position = new Vector2(100, 200);
 
             var opiece = myScene.createEntity("opiece");
             opiece.addComponent<OPiece>();
+            opiece.addComponent(new Sprite(tetromino));
+            opiece.position = new Vector2(100, 250);
 
             var spiece = myScene.createEntity("spiece");
             spiece.addComponent<SPiece>();
+            spiece.addComponent(new Sprite(tetromino));
+            spiece.position = new Vector2(100, 300);
 
             var tpiece = myScene.createEntity("tpiece");
             tpiece.addComponent<TPiece>();
+            tpiece.addComponent(new Sprite(tetromino));
+            tpiece.position = new Vector2(100, 350);
 
             Core.scene = myScene;
         }
