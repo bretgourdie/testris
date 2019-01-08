82bf2c88176b373608311952124a5f754bdcbe42
Bret Gourdie <BretGourdie@gmail.com>
2019-01-07 22:39:49 -0500

    Added RotationDirection

---
+++ Testris/GameLogic/RotationDirection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testris.GameLogic
+{
+    public enum RotationDirection
+    {
+        Clockwise,
+        Counterclockwise
+    }
+}
