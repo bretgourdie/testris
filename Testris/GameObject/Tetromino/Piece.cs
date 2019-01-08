90361032b9fd9ff4d2340e50ac0097f047126755
Bret Gourdie <BretGourdie@gmail.com>
2019-01-07 22:21:47 -0500

    Added better arrangement logic and rotation

--- Testris/GameObject/Tetromino/Piece.cs
+++ Testris/GameObject/Tetromino/Piece.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Nez;
+using Testris.GameLogic;
 
 namespace Testris.GameObject.Tetromino
 {
@@ -12,14 +13,52 @@ namespace Testris.GameObject.Tetromino
     {
         public abstract Color Color { get; }
 
-        public int[,] CurrentArrangement { get; protected set; }
+        public List<int[,]> ClockwiseArrangements { get; protected set; }
 
         public Piece() : base()
         {
-            CurrentArrangement = getInitialArrangement();
+            ClockwiseArrangements = createClockwiseArrangements();
+            currentArrangementIndex = 0;
         }
 
-        protected abstract int[,] getInitialArrangement();
+        public int[,] GetCurrentArrangement()
+        {
+            return ClockwiseArrangements[currentArrangementIndex];
+        }
 
+        public void Rotate(RotationDirection direction)
+        {
+            int indexIncrementor;
+
+            switch (direction)
+            {
+                case RotationDirection.Clockwise:
+                    indexIncrementor = 1;
+                    break;
+                case RotationDirection.Counterclockwise:
+                    indexIncrementor = -1;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            var desiredIndex = (currentArrangementIndex + 1 * indexIncrementor) % ClockwiseArrangements.Count;
+
+            var desiredArrangement = ClockwiseArrangements[desiredIndex];
+
+            if (canRotate(desiredArrangement))
+            {
+                currentArrangementIndex = desiredIndex;
+            }
+        }
+
+        protected abstract List<int[,]> createClockwiseArrangements();
+
+        private int currentArrangementIndex;
+
+        private bool canRotate(int[,] desiredArrangement)
+        {
+            return true;
+        }
     }
 }
