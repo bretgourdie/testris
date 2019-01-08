c66d6e867917fbd593aa211473cc4eb85eb0eeb7
Bret Gourdie <bretgourdie@gmail.com>
2019-01-08 15:58:58 -0500

    Added Matrix

---
+++ Testris/GameObject/Matrix.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testris.GameObject.Tetromino;
+
+namespace Testris.GameObject
+{
+    public class Matrix
+    {
+        public int[,] Grid = new int[40, 10];
+
+        private const int startingX = 4;
+        private const int startingY = 21;
+    }
+}
