1218ea28ccf13d1c1fe195f502e658f5baeb45a7
Bret Gourdie <BretGourdie@gmail.com>
2019-01-10 23:40:25 -0500

    Added Mino
    I guess??

---
+++ Testris/GameObject/Mino.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nez;
+using Nez.Sprites;
+
+namespace Testris.GameObject
+{
+    public class Mino : Component
+    {
+        private Sprite _sprite { get; set; }
+
+        public virtual void onAddedToEntity()
+        {
+            _sprite = entity.getComponent<Sprite>();
+        }
+    }
+}
